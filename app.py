5334691e667a0cdc416d9020eab4afd2e654c0a4
satish ganiyada <90520002+Satishganiyada@users.noreply.github.com>
2025-11-05 12:26:38 +0530

    Update home route message for AWS App Runner

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask Web Application!"
+    return "Hello, Flask Web Application with AWS App Runner!"
 
 @app.route("/about")
 def about():
@@ -12,3 +12,4 @@ def about():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
